8d5c487c05040476a64e0e22cc12545cab73890d
Shivansh Verma <shivanshverma082004@gmail.com>
2022-04-03 10:05:02 +0530

    Model fix 2

--- practice/models.py
+++ practice/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
 class Users(models.Model):
@@ -15,5 +16,5 @@ class Transactions(models.Model):
     price = models.FloatField()
     symbol = models.CharField(max_length=10)
     type = models.CharField(max_length=20)
-    time = models.DateTimeField(auto_now_add=True)
+    time = models.DateTimeField(default = datetime.now, auto_now_add=True)
 
