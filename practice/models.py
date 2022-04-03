38a1360d0d571e6cb85d38d26b178cd3c23d4a02
Shivansh Verma <shivanshverma082004@gmail.com>
2022-04-03 10:37:40 +0530

    Model Fix

--- practice/models.py
+++ practice/models.py
@@ -1,10 +1,4 @@
 from django.db import models
-from django.db.models import DateTimeField
-
-class DateTimeWithoutTZField(DateTimeField):
-    def db_type(self, connection):
-        return 'timestamp'
-
 # Create your models here.
 class Users(models.Model):
     id = models.AutoField(primary_key=True)
@@ -20,5 +14,5 @@ class Transactions(models.Model):
     price = models.FloatField()
     symbol = models.CharField(max_length=10)
     type = models.CharField(max_length=20)
-    time = DateTimeWithoutTZField(auto_now=True)
+    time = models.DateField(auto_now_add=True)
 
