e3529b0057d9cd1f5fd9180a826ae8f1a2e0f43e
Shivansh Verma <shivanshverma082004@gmail.com>
2022-04-03 10:05:53 +0530

    Model Fix

--- practice/models.py
+++ practice/models.py
@@ -16,5 +16,5 @@ class Transactions(models.Model):
     price = models.FloatField()
     symbol = models.CharField(max_length=10)
     type = models.CharField(max_length=20)
-    time = models.DateTimeField(default = datetime.now, auto_now_add=True)
+    time = models.DateTimeField(default = datetime.now)
 
