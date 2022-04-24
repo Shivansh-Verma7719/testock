f62271916ee36d0bd2d09e6925c6b7783a63e092
Shivansh Verma <shivanshverma082004@gmail.com>
2022-04-24 21:59:43 +0530

    Bug fix

--- practice/models.py
+++ practice/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils import timezone
+
 # Create your models here.
 class Users(models.Model):
     id = models.AutoField(primary_key=True)
@@ -14,5 +16,5 @@ class Transactions(models.Model):
     price = models.FloatField()
     symbol = models.CharField(max_length=10)
     type = models.CharField(max_length=20)
-    time = models.DateField(auto_now_add=True)
+    time = models.DateField(auto_now=timezone.now())
 
